0d5e087b0030c874b31830c51f6782deb14f8e16
tuanna <tuan.a1.bt@gmail.com>
2025-03-19 17:10:24 +0700

    fix(dp): add missing config

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,8 @@
 import appRouter from "@/server"
 import { handle } from "hono/vercel"
 
+export const runtime = "edge"
+
 // This route catches all incoming API requests and lets your appRouter handle them.
 export const GET = handle(appRouter.handler)
 export const POST = handle(appRouter.handler)
