59b1dcc499bb91e5910a707b194c8ea210565364
tuanna <tuan.a1.bt@gmail.com>
2025-04-15 23:13:51 +0700

    fix types

--- src/shared/providers/react-query.tsx
+++ src/shared/providers/react-query.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { getQueryClient } from "@/shared/utils/get-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import type * as React from "react";
+import type { ReactNode } from "react";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
